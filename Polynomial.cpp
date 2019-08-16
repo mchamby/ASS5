00aeae01813d7e62136b0b2e5ec29abea483ca5b
mchamby <53239708+mchamby@users.noreply.github.com>
2019-08-16 12:21:13 -0400

    Fix the coefficient parsing

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -1,36 +1,58 @@
-#include "stdafx.h"
+//#include "stdafx.h"
 #include "Polynomial.h"
 
 
 Polynomial::Polynomial(string input)
 {
-	const int maxNumOfSegments = 100;
-
-	// separating the strings into segments
+	const int maxNumOfSegments = 10;
 	string segments[maxNumOfSegments] = { "" }; // the element of the segment corresponds to the power of x
+	
+	// separating the input string into segments
 	while (input.empty() == false)
 	{
 		int first = input.find_last_of("+-");
-		int powerOfX = input.back();
-		segments[powerOfX] = input.substr(first);
-		cout << segments[powerOfX] << " ";
-		input.erase(first);
+		int powerOfX = (input.back() - 48);	// this only works for 1 digit powers
+		segments[powerOfX].assign(input.substr(first));
+		cout << segments[powerOfX] << " ";	// printing the parsed segment
+		input.erase(first);	// erase the parsed segment from the input string
 	}
 
-	/*
-	int counter = 0;
+	// put the coefficients into the coefficients vector
 	for (int i = 0; i < maxNumOfSegments; ++i)
 	{
+		// check if there is a string in this element
+		// we don't want to perform operations on an empty string!
 		if (segments[i].empty() == false)
 		{
-			int first = segments[i].find_first_of();
+			coefficients.push_back(getValueAfterSign(segments[i]));
 		}
+		else coefficients.push_back(0.0);
+	}
 
-		// when there is no recorded
-	}*/
+	cout << '\n';
+	// printing all coefficients
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << coefficients[i] << ' ';
+	}
 }
 
 
 Polynomial::~Polynomial()
 {
+}
+
+int Polynomial::getValueAfterX(string segment) const
+{
+	int xIndex = segment.find('x');
+	return segment[xIndex + 1];
+}
+
+double Polynomial::getValueAfterSign(string segment) const
+{
+	int signIndex = segment.find("+-");
+	int xIndex = segment.find('x');
+	string valueString = segment.substr(signIndex + 1, xIndex - signIndex);
+	double value = stod(valueString);
+	return value;
 }
--- Polynomial.h
+++ Polynomial.h
@@ -10,6 +10,8 @@ class Polynomial
 public:
 	Polynomial(string input);
 	~Polynomial();
+	int getValueAfterX(string segment) const;
+	double getValueAfterSign(string segment) const;
 private:
 	vector<double> coefficients; // the element of the vector corresponds to the power of x
 };
