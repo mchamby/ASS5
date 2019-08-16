898aceb4b6ea97fa60ec2573ea078cee36c00127
littleAndy96 <53239477+littleAndy96@users.noreply.github.com>
2019-08-16 16:36:13 -0400

    More of The Stuff

--- Rational.cpp
+++ Rational.cpp
@@ -2,8 +2,19 @@
 #include "Rational.h"
 
 
-Rational::Rational()
+Rational::Rational(string polynomial1,string polynomial2)
 {
+    Polynomial p1(polynomial1);
+    Polynomial p2(polynomial2);
+    if(p2 == 0)
+
+}
+
+Rational::Rational(Polynomial& polynomial1,string polynomial2)
+{
+    Polynomial p1 = polynomial1;
+    Polynomial p2(polynomial2);
+    
 }
 
 
