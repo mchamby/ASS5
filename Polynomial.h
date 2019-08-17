2ee69ee0b17de39e54e32fe96bd53945f6356efc
jonfodi <48962228+jonfodi@users.noreply.github.com>
2019-08-17 13:24:08 -0400

    Add files via upload
    headers for the overloads

--- Polynomial.h
+++ Polynomial.h
@@ -2,26 +2,19 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <exception>
-#include "Exception.h"
-#include "DivisionByZero.h"
-#include "InvalidPower.h"
-#include "InvalidString.h"
 
 using namespace std;
 
 class Polynomial
 {
-	friend ostream& operator << (ostream& os, const Polynomial& rhs);
 public:
 	Polynomial(string input);
-	Polynomial(const Polynomial& rhs);
+
 	~Polynomial();
-	void testForInvalidString(string input);
-	void testForInvalidPower(string input);
-  double operator() (double x) const;
+
+	int getValueAfterX(string segment) const;
+	double getValueAfterSign(string segment) const;
+
 private:
 	vector<double> coefficients; // the element of the vector corresponds to the power of x
 };
-
