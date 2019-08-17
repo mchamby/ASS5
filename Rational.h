00cb10a1fbf15f5187c72a86a570da54ef9b2090
mchamby <53239708+mchamby@users.noreply.github.com>
2019-08-17 13:59:46 -0400

    Add () operator overload

--- Rational.h
+++ Rational.h
@@ -9,6 +9,8 @@ public:
 	Rational(string polynomial1, Polynomial& polynomial2);
 	Rational(Polynomial& polynomial1, string polynomial2);
 	Rational(Polynomial& polynomial1, Polynomial& polynomial2);
+  double operator() (double x) const;
+
 private:
 	Polynomial numerator;
 	Polynomial denominator;
